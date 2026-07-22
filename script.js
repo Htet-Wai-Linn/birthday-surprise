ad6c5f5420041a6b7db884654f15281a7e894e7e
Htet-Wai-Linn <57446172+Htet-Wai-Linn@users.noreply.github.com>
2026-07-22 18:13:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+function surprise() {
+    alert("🎂 Happy Birthday 🎂\n\n🤍 ကောင်မလေး 🤍\n\nမွေးနေ့မှာ ပျော်ရွှင်ပါစေ ❤️");
+}
 
+
+// Floating hearts
+setInterval(() => {
+    let heart = document.createElement("div");
+    heart.innerHTML = "❤️";
+    heart.style.position = "absolute";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.bottom = "0";
+    heart.style.fontSize = "25px";
+    heart.style.animation = "float 5s linear";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+
+}, 500);
